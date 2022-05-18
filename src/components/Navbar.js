358b4a6b9ac7b50464ed6f0e358feab23f94258d
Adarsha Halder <pradiphalder70@gmail.com>
2022-05-18 21:38:01 +0530

    footer updated

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './nav.css'
+
+ const Navbar =()  => {
+
+    return(
+        <>
+        <nav className="main-nav">
+            <div className="logo">
+         <h1>school of Engineering</h1>
+            </div>
+            {/*2ND PART MENU */}
+            <div className="connect">
+                <h2>connect with us</h2>
+
+            </div>
+
+
+        </nav>
+        </>
+    );
+ };
+ export default Navbar;
